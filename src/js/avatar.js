d4eaf524378cc8776ddd8262f80c42c2f5f25c33
Aleksandr Nekrasov <snake_nas@mail.ru>
2025-03-09 22:48:26 +0700

    animate movements

--- src/js/avatar.js
+++ src/js/avatar.js
@@ -7,100 +7,99 @@ class Avatar {
     lastBlinkTime = 0;
     blinkCooldown = 5000;
     isSleep = true
-
+    mixer;
 
     constructor(modelUrl) {
 
         this.clock  = new THREE.Clock();
-        this.fbxLoader = new THREE.FBXLoader(); // Loader for FBX
-
         this.scene = new THREE.Scene();
-        this.camera = new THREE.PerspectiveCamera(15, window.innerWidth / window.innerHeight, 0.01, 100);
-        this.camera.position.set(0, 0, 2);
-        //this.camera = new THREE.PerspectiveCamera(65, window.innerWidth / window.innerHeight, 0.1, 10);
-        //this.camera.position.set(0, 0, 5);
+
+        this.camera = new THREE.PerspectiveCamera(7, window.innerWidth / window.innerHeight, 0.01, 100);
+        this.camera.position.set(0, 2.0, 5);
+        this.camera.lookAt(new THREE.Vector3(0, 1.7, 0));
+
 
         this.renderer = new THREE.WebGLRenderer({antialias: true, alpha: true});
+        // Enable shadows in the renderer
+        this.renderer.shadowMap.enabled = true;
+        this.renderer.shadowMap.type = THREE.PCFSoftShadowMap; // Softer shadows
 
-        const ambientLight = new THREE.AmbientLight(0xffffff, 1.5);
+        const ambientLight = new THREE.AmbientLight(0xffffff, 1.7);
         this.scene.add(ambientLight);
 
-        const directionalLight = new THREE.DirectionalLight(0xffffff, 1.2);
-        directionalLight.position.set(100, 100, 100).normalize();
-        this.scene.add(directionalLight);
+        // //SpotLight for more focused highlights
+        const spotlight = new THREE.SpotLight(0x88ccff, 1.5); // Soft blueish tint
+        spotlight.position.set(10, 10, 10); // Position above and slightly in front
+        spotlight.angle = 0.15; // Narrow focus
+        spotlight.penumbra = 1; // Soft edges
+        spotlight.decay = 2; // Natural light falloff
+        spotlight.distance = 10; // Limit range
+        spotlight.castShadow = true; // Enable shadows
+        spotlight.shadow.bias = -0.003; // Reduce shadow artifacts
+        this.scene.add(spotlight);
 
-        const controls = new THREE.OrbitControls(this.camera, this.renderer.domElement);
-        controls.enableDamping = true;
-        controls.dampingFactor = 0.05;
+        const coolFillLight = new THREE.HemisphereLight(0x88ccff, 0x080820, 0.5); // Sky blue & dark blue
+        this.scene.add(coolFillLight);
+
+     //  const controls = new THREE.OrbitControls(this.camera, this.renderer.domElement);
+     //  controls.enableDamping = true;
+     //  controls.dampingFactor = 0.05;
 
         const loader = new THREE.GLTFLoader();
         const clz = this
-
-        this.isAnimationsLoaded = false; // Flag to check if animations are loaded
 
         // https://demo.readyplayer.me/avatar?id=67bd3923758ea27bb2ef2f37
         loader.load(
             modelUrl,
             function (gltf) {
-                clz.avatar = gltf.scene;
-                clz.avatar.position.set(0, -1.7, 0);
-                //clz.avatar.position.set(0, 0, 0);
-                clz.scene.add(clz.avatar);
-                clz.mouthMesh = clz.avatar.getObjectByName('Mouth'); // Assuming this is the mouth mesh
+
+                const model = gltf.scene;
+                clz.avatar = model;
+                clz.mouthMesh = model.getObjectByName('Mouth'); // Assuming this is the mouth mesh
                 clz.avatar.traverse(function (child) {
                     if (child.isMesh) {
-                        console.log(child.name, child.position, child.scale);
+                       // console.log(child.name, child.position, child.scale);
                     }
                     if (child.name.toLowerCase() === "head") clz.head = child;
                     if (child.name.toLowerCase() === "lefteye") clz.leftEye = child;
-
                     if (child.name.toLowerCase() === "righteye") clz.rightEye = child;
 
                     if (child.name === 'Wolf3D_Avatar') {
-                        console.log(child)
                         clz.wolfAvatar = child;
                         clz.wolfAvatar.morphTargetInfluences[clz.wolfAvatar.morphTargetDictionary.mouthSmile] = 0.3
                     }
                 });
+                model.position.set(0, 0, 0);
+                model.scale.set(1, 1, 1);
+                clz.scene.add(model);
 
-                if (clz.leftEye && clz.rightEye) {
-                    console.info('Left and Right eyes found!');
-                }
 
+
+                const fbxLoader = new THREE.FBXLoader();
+                fbxLoader.load('./src/assets/F_Standing_Idle_001.fbx', (fbx) => {
+                    clz.mixer = new THREE.AnimationMixer(model);
+                    const clip = fbx.animations[0]
+                    const tracks = clip.tracks;
+                    const validTracks = tracks.filter(track => {
+                        return  !track.name.includes("Hips.position")
+                    });
+
+                    const filteredClip = new THREE.AnimationClip('FilteredAnimation', -1, validTracks);
+                    const action = clz.mixer.clipAction(filteredClip)
+                    action.play(); // Start the animation
+
+                });
                 // https://docs.readyplayer.me/ready-player-me/integration-guides/unity/animations/ready-player-me-animation-library
                 // https://github.com/readyplayerme/animation-library/tree/master?tab=readme-ov-file#tutorials
-                clz.loadAnimations(clz.avatar, "./src/assets/F_Standing_Idle_001.fbx");
-
             },
             undefined,
             function (error) {
                 console.error('Error loading GLB file:', error);
             }
         );
-        this.lastTime = 0;  // Store last frame time
         this.animate();
     }
 
-    loadAnimations(avatar, animationUrl) {
-        const clz = this;
-        this.fbxLoader.load(animationUrl, function (fbxAnimation) {
-            const animations = fbxAnimation.animations;
-            if (animations && animations.length > 0) {
-                // Initialize the mixer with the avatar model
-                clz.mixer = new THREE.AnimationMixer(avatar);
-
-                animations.forEach((clip) => {
-                    const action = clz.mixer.clipAction(clip);
-                    action.play();
-                });
-
-                // Set the flag to true when animations are loaded
-                clz.isAnimationsLoaded = true;
-            } else {
-                console.warn("No animations found in the FBX file.");
-            }
-        });
-    }
 
     animate() {
         const clz = this
@@ -108,18 +107,14 @@ class Avatar {
             clz.animate()
         });
 
+        // update animations
+        if (clz.mixer) clz.mixer.update(clz.clock.getDelta());
         this.animateMouthSmoothly();
         this.animateEyes()
-
-        if (this.isAnimationsLoaded && this.mixer) {
-           // this.mixer.update(this.clock.getDelta()); // FIX
-        }
-
-
         this.renderer.render(this.scene, this.camera);
-
     }
 
+    // blink eyes, or close if disconected
     animateEyes() {
         if (this.isSleep && this.leftEye && this.rightEye) {
             this.leftEye.scale.y = this.rightEye.scale.y = 0.0;
@@ -139,6 +134,7 @@ class Avatar {
         }
     }
 
+    // animate speech
     animateMouthSmoothly() {
         if (this.wolfAvatar && this.wolfAvatar.morphTargetDictionary) {
             this.wolfAvatar.mouthSmileLeft = 1
